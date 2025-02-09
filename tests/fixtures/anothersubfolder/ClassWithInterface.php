20f4f8b8c4ab549e09fcd8ae68fe17d480bcd8f5
Wouter de Jong <wj@dejo.dev>
2025-02-09 20:33:05 +0100

    Fixed tests

--- tests/fixtures/anothersubfolder/ClassWithInterface.php
+++ tests/fixtures/anothersubfolder/ClassWithInterface.php
@@ -4,4 +4,17 @@ namespace DeJoDev\Fabriek\Fixtures\AnotherSubFolder;
 
 use DeJoDev\Fabriek\Fixtures\MyInterface;
 
-class ClassWithInterface implements MyInterface {}
+class ClassWithInterface implements MyInterface
+{
+    private bool $fooCalled = false;
+
+    public function foo(): void
+    {
+        $this->fooCalled = true;
+    }
+
+    public function fooCalled(): bool
+    {
+        return $this->fooCalled;
+    }
+}
